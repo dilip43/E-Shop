d8349d3f3ece971dd81ccaf8f31cf23556aac64c
Dilip <84269197+dilip43@users.noreply.github.com>
2023-07-27 18:15:04 +0530

    Update shop.js

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -40,7 +40,7 @@ router.post(
 
 			const activationToken = createActivationToken(seller);
 
-			const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+			const activationUrl = `https://e-shop-x2xi.vercel.app/activation/${activationToken}`;
 
 			try {
 				await sendMail({
